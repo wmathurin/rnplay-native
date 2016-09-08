b10badf800eb63e573d321f3dc0fd7832fb874f1
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-09-08 14:46:26 -0700

    Adding pods for mobile sdk (except for salesforce react) NBL workspace builds and runs

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Getting git submodules"
+git submodule init
+git submodule sync
+git submodule update --init --recursive
+
+echo "Installing npm dependencies"
+npm install
+
+echo "Installing ios pods"
+cd ios
+pod update
+cd ..
+
